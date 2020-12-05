5056072c447f6ae778166ea734b27e0cc3e3bc89
bootspace <gregudogu@gmail.com>
2020-12-05 01:41:30 +0100

    Added a description to the package.json file

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors =require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+require('dotenv').config();
+
+//Initializing the express App
+const app = express();
+const port = process.env.PORT || 5000;
+
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+//Settin up some middlewares
+//Cors setup
+app.use(cors());
+
+//bodyParser setup
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Connecting to the MongoDB database
+const url = process.env.ATLAS_URL;
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB Database succesfully connected');
+})
+
+//Linking up our route files
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter);
+
+// Connecting to SErver
+app.listen(port,() => {
+  console.log(`Server is running on Port: ${port}`);
+});
